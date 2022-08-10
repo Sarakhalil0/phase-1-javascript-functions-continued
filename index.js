1128474af9f82ed7cb66cbc501623ad95411d4d6
sara khalil <sarakhalil3377@gmail.com>
2022-08-10 15:37:56 +0300

    commit tree c3771b2ee5038ccf90415568bd33318a11bc25d3

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+//first
+function saturdayFun(activity = "roller-skate") {
+    return (`This Saturday, I want to ${activity}!`);
+}
+saturdayFun();
+saturdayFun("bathe my dog");
+
+//second
+const mondayWork = function (activity = "go to the office") {
+    return (`This Monday, I will ${activity}.`);
+}
+mondayWork();
+mondayWork("work from home");
+
+//third
+
